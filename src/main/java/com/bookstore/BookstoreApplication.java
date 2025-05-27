7b7fa787af5293e6aefc3690268850bf4a75c4ff
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-05-27 13:31:26 +0530

    updated

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -2,28 +2,25 @@ package com.bookstore;
 
 import com.bookstore.resource.*;
 import com.bookstore.exception.GenericExceptionMapper;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
+import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/api")
-public class BookstoreApplication extends Application {
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        classes.add(RootResource.class);
-        classes.add(BookResource.class);
-        classes.add(AuthorResource.class);
-        classes.add(CustomerResource.class);
-        classes.add(CartResource.class);
-        classes.add(OrderResource.class);
-        classes.add(GenericExceptionMapper.class);
-        classes.add(CorsFilter.class); // Add CORS filter
-        classes.add(JacksonFeature.class); // Ensure JSON processing
-        return classes;
+public class BookstoreApplication extends ResourceConfig {
+    public BookstoreApplication() {
+        packages("com.bookstore.resource"); // Optional, if you're not manually adding each class
+        register(RootResource.class);
+        register(BookResource.class);
+        register(AuthorResource.class);
+        register(CustomerResource.class);
+        register(CartResource.class);
+        register(OrderResource.class);
+        register(GenericExceptionMapper.class);
+        register(CorsFilter.class); // This will now work more reliably
+        register(JacksonFeature.class);
+        register(RolesAllowedDynamicFeature.class);
     }
 }
